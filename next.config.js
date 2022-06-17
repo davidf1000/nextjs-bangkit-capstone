27e1f546a233e77be6b6b1b1ab5c0849b097c953
davidf1000 <davidfauzi29@gmail.com>
2022-06-17 16:11:02 +0700

    edit config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        // BASEPATH: 'https://ecotrans-webapp-h3lwczj22a-et.a.run.app',
-        BASEPATH: 'http://localhost:3000',
+        BASEPATH: 'https://ecotrans-webapp-h3lwczj22a-et.a.run.app'
+        // BASEPATH: 'http://localhost:3000',
     },
   }
